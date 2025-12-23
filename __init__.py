737b43d702b36859065363a6662f85f6f3e58741
jenniferyxlin <jenniferyxlin@gmail.com>
2025-12-23 02:15:46 -0800

    Delete unused files

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ from .model import MDLModel, RotaryEmbedding
 from .diffusion import get_masked_batch, masked_cross_entropy_loss
 
 __version__ = "0.2.0"
-__all__ = ['MDLModel', 'RotaryEmbedding', 'get_masked_batch', 'masked_cross_entropy_loss']
+__all__ = ['MDLModel', 'RotaryPositionalEmbedding', 'get_masked_batch', 'masked_cross_entropy_loss']
